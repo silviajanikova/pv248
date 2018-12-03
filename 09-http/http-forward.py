055965811d7a19d3c51e961428554e85276e0cd9
silviajanikova <sis.janikova@gmail.com>
2018-12-03 21:42:51 +0100

    exercise 09 edited

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -9,8 +9,10 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, urlparse
+from urllib.error import HTTPError, URLError
 
 from socket import timeout
+
 
 TIMEOUT = 1
 CODE_OK = 200
@@ -36,10 +38,17 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
 	# get code/headers/json
 	def do_GET(self):
-		
 		resp_client = {}
 		try:
-			with urlopen(url, timeout=TIMEOUT) as resp:
+			headers = dict(self.headers)
+			headers['Accept-Encoding'] = 'identity'
+			del headers["Host"]
+
+			request = Request(url, 
+				method='GET', 
+				headers=headers)
+
+			with urlopen(request, timeout=TIMEOUT) as resp:
 				resp_client['code'] = resp.getcode()
 				resp_client['headers'] = dict(resp.getheaders())
 				content = resp.read().decode('utf-8')
@@ -53,6 +62,8 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 			resp_client['code'] = 'timeout'
 
 		self.send_response(CODE_OK)
+		self.send_header('Connection', 'close')
+		self.send_header('Content-Type', 'application/json')
 		self.end_headers()
 		self.wfile.write(bytes(json.dumps(resp_client), 'utf-8'))
 
@@ -65,6 +76,7 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
 		if self.json_validator(encoded_data):
 			data_dict = json.loads(encoded_data)
+			
 			if 'type' not in data_dict:
 				data_dict['type'] = 'GET'
 
@@ -73,37 +85,38 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 				custom_timeout = int(data_dict['timeout'])
 
 			request = Request(data_dict['url'], 
-				headers=data_dict['headers'], 
 				method=data_dict['type'])
+
+			if 'headers' in data_dict and type(data_dict['headers']) is dict:
+				request.headers = data_dict['headers']
 			
 			if data_dict['type'] == 'POST':
-				request.data = data_dict['content']
+				request.data = bytes(data_dict['content'], 'utf-8')
 
 			try:
-				print("1")
 				with urlopen(request, timeout=custom_timeout) as resp:
-					print("1")
-
 					resp_client['code'] = resp.getcode()
 					resp_client['headers'] = dict(resp.getheaders())
 					content = resp.read().decode('utf-8')
-					print("1")
 					try:
 						resp_client['json'] = json.loads(content)
 					except:
 						resp_client['content'] = content
-					print("1")
-
+			except HTTPError as error:
+				resp_client['code'] = error.code
+			except URLError as error:
+				resp_client['code'] = 'invalid json'
 			except timeout:
 				resp_client['code'] = 'timeout'
-			except:
-				resp_client['code'] = 'invalid json'
+			# except:
+				# resp_client['code'] = 'invalid json'
 
 
 		else:
 			resp_client['code'] = 'invalid json'
 		
 		self.send_response(CODE_OK)
+		self.send_header('Content-Type', 'application/json')
 		self.end_headers()
 		self.wfile.write(bytes(json.dumps(resp_client), 'utf-8'))
 		
@@ -112,6 +125,7 @@ def check_url(url):
 	o = urlparse(url)
 	if o.scheme == '':
 		url = 'http://' + url
+		url += ':80'
 	return url
 
 
@@ -122,7 +136,6 @@ if len(sys.argv) != 3:
 # parse arguments
 port = int(sys.argv[1])
 url = check_url(sys.argv[2])
-
 
 httpd = HTTPServer(('localhost', port), CustomHTTPRequestHandler)
 httpd.serve_forever()
